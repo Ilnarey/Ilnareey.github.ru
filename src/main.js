c06f249e33cc1a42c5db239c5d5781db4e58ebd2
Ilnarey <ilnaragaliullina16@gmail.com>
2024-10-12 21:50:15 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import HeaderComponent from './view/header-component.js';
 import FormComponent from './view/form-component.js';
-import TaskBlockComponent from './view/taskBlock-component.js';
-
+import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import {render, RenderPosition} from './framework/render.js';
-
+import TasksModel from './model/task-model.js';
 
 const bodyContainer= document.querySelector('.header');
 const formContainer= document.querySelector('.new_task');
 const taskBlockContainer= document.querySelector('.tasks');
 
-
+const tasksModel = new TasksModel();
+const tasksBoardPresenter = new TasksBoardPresenter({
+    boardContainer: taskBlockContainer, 
+    tasksModel,
+});
 
 
 render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
 render(new FormComponent(), formContainer, RenderPosition.AFTERBEGIN);
-for (var i =1;i<=4;i++){
-    render(new TaskBlockComponent(), taskBlockContainer, RenderPosition.AFTERBEGIN);
-}
+tasksBoardPresenter.init();
