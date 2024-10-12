b6a82a2aca4dfb6ff771464948b5f0e6f7fd28cf
Ilnarey <ilnaragaliullina16@gmail.com>
2024-10-12 21:39:44 +0500

    Create button-delite.js

---
+++ src/view/button-delite.js
@@ -0,0 +1,22 @@
+import { createElement } from "../framework/render.js"; 
+ 
+function createClearButtonComponentTemplate() { 
+  return ` 
+                <button type="button">x Очистить</button> 
+        `; 
+} 
+ 
+export default class ClearButtonComponent{ 
+  getTemplate() { 
+    return createClearButtonComponentTemplate(); 
+  } 
+  getElement() { 
+    if (!this.element) { 
+      this.element = createElement(this.getTemplate()); 
+    } 
+    return this.element; 
+  } 
+  removeElement() { 
+    this.element = null; 
+  } 
+}
