89c581c957106a441145970b243775a858abde0f
Ilnarey <ilnaragaliullina16@gmail.com>
2024-10-12 21:48:37 +0500

    Create const.js

---
+++ src/const.js
@@ -0,0 +1,15 @@
+export const Status = {
+    BACKLOG: `blogack`,
+    PROCESSING: `in_progress`,
+    DONE: `ready`,
+    BASKET: `garbage`,
+  };
+  
+
+
+export const StatusLabel = {
+    [Status.BACKLOG]: `Бэклог`,
+    [Status.PROCESSING]: `В процессе`,
+    [Status.DONE]: `Готово`,
+    [Status.BASKET]: `Корзина`,
+  };
