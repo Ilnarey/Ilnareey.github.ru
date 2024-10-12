9c47d14d3512a2cfc3e1de13eb85396094929594
Ilnarey <ilnaragaliullina16@gmail.com>
2024-10-12 21:29:34 +0500

    Create task.js

---
+++ src/mock/task.js
@@ -0,0 +1,47 @@
+export const tasks = [
+    {
+    'id': '1',
+    'title': 'Выучить JS',
+    'status': 'blogack',
+    },
+    {
+     'id': '2',
+    'title': 'Выучить React',
+    'status': 'blogack',   
+    },
+    {
+        'id': '3',
+       'title': 'Сделать домашку',
+       'status': 'blogack',   
+    },
+    {
+        'id': '4',
+       'title': 'Выпить смузи',
+       'status': 'in_progress',   
+    },
+    {
+        'id': '5',
+       'title': 'Выпить воды',
+       'status': 'in_progress',   
+    },
+    {
+        'id': '6',
+       'title': 'Позвонить маме',
+       'status': 'ready',   
+    },
+    {
+        'id': '7',
+       'title': 'Погладить кота',
+       'status': 'ready',   
+    },
+    {
+        'id': '8',
+       'title': 'Сходить погулять',
+       'status': 'garbage',   
+    },
+    {
+        'id': '9',
+       'title': 'Прочитать книгу',
+       'status': 'garbage',   
+    }
+]
