26df48f67c1eb0a1fa2ac3ab0da40572579237d2
Ilnarey <ilnaragaliullina16@gmail.com>
2024-10-17 00:55:18 +0500

    Update tasks-list-component.js

--- src/view/tasks-list-component.js
+++ src/view/tasks-list-component.js
@@ -1,37 +1,26 @@
-import {createElement} from '../framework/render.js';
+import { AbstractComponent } from "../framework/view/abstract-component.js";
 import {StatusLabel } from '../const.js';
 
 function createTasksListTemplate(status) {
     return (
       
       `
-              <div class="${status}">
-                 <label>${StatusLabel[status]}</label>
-              </div>
+              <ul class="${status}">
+                <label>${StatusLabel[status]}</label>
+              </ul>
 
     `
 
       );
 }
 
-export default class tasksListComponent {
+export default class tasksListComponent extends AbstractComponent{
 constructor({status}){
+  super();
   this.status=status;
 }
 
-  getTemplate() {
-    return createTasksListTemplate(this.status);
-  }
-
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-
-    return this.element;
-  }
-
-  removeElement() {
-    this.element = null;
+get template() { 
+  return createTasksListTemplate(this.status);
   }
 }
