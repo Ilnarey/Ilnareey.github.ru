200e5a6cd2c4e20a3dcd0a8b2db0eecff2b621d9
Ilnarey <ilnaragaliullina16@gmail.com>
2024-10-17 00:51:19 +0500

    Update tasks-board-presenter.js

--- src/presenter/tasks-board-presenter.js
+++ src/presenter/tasks-board-presenter.js
@@ -2,46 +2,53 @@ import TasksListComponent from '../view/tasks-list-component.js';
 import TaskComponent from '../view/task-component.js';
 import TaskBoardComponent from '../view/tasks-board-component.js';
 import {render} from '../framework/render.js';
-import { Status, StatusLabel } from '../const.js';
+import { Status } from '../const.js';
 import ClearButtonComponent from "../view/button-delite.js";
 
 
 export default class TasksBoardPresenter {
- tasksBoardComponent = new TaskBoardComponent();
-
+  #boardContainer=null;
+  #tasksModel=null;
+  #boardTasks=[];
+  #tasksBoardComponent = new TaskBoardComponent()
 
  constructor({boardContainer, tasksModel}) {
-   this.boardContainer = boardContainer;
-   this.tasksModel =  tasksModel;
+   this.#boardContainer = boardContainer;
+   this.#tasksModel =  tasksModel;
  }
 
 init() {
-  this.boardTasks = [...this.tasksModel.getTasks()];
- render(this.tasksBoardComponent, this.boardContainer);   
+  this.#boardTasks = [...this.#tasksModel.getTasks()];
+ render(this.#tasksBoardComponent, this.#boardContainer);  
+
  for (let i in Status) {
   this.status_title = Status[i];
   const tasksListComponent = new TasksListComponent({status: Status[i]});
-     render(tasksListComponent, this.tasksBoardComponent.getElement());
-    
-     for (let j = 0; j <  this.boardTasks.length; j++) {
-         const taskComponent = new TaskComponent({task: this.boardTasks[j]});
-         if (this.boardTasks[j].status == this.status_title)
-         {
-         render(taskComponent, tasksListComponent.getElement());
-         }
-     }
+    render(tasksListComponent, this.#tasksBoardComponent.element);
+    if (tasksListComponent.length===0){
+      render(new SubmitTaskComponent(), tasksListComponent.element());
     }
- this.makeClearButton();
- 
+    else{
+      for (let j = 0; j <  this.#boardTasks.length; j++) {
+        this.#renderTask(
+          this.#boardTasks[j],
+          tasksListComponent.element,
+          this.status_title)
+      }
+    }
+  }
+this.makeClearButton();
 }
-
- makeClearButton() { 
-  const basketContainer = document.querySelector(`.${Status.BASKET}`); 
-   
-  render(new ClearButtonComponent(), basketContainer);
+#renderTask(task, container, status_title) {
+  const taskComponent = new TaskComponent({ task: task });
+  if (task.status == status_title) {
+    render(taskComponent, container);
+  }
+}  
+makeClearButton() {
+const basketContainer = document.querySelector(`.${Status.BASKET}`);
+render(new ClearButtonComponent(), basketContainer);
 }
 }
-
-
 
 
