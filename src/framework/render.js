84c1a44bf544462cd32822ef11564fbbcc73d034
Ilnarey <ilnaragaliullina16@gmail.com>
2024-10-17 00:49:54 +0500

    Update render.js

--- src/framework/render.js
+++ src/framework/render.js
@@ -1,25 +1,30 @@
+import { AbstractComponent } from "../framework/view/abstract-component.js";
+
+
 const RenderPosition = {
-  BEFOREBEGIN: 'beforebegin',
-  AFTERBEGIN: 'afterbegin',
-  BEFOREEND: 'beforeend',
-  AFTEREND: 'afterend',
+    BEFOREBEGIN: 'beforebegin',
+    AFTERBEGIN: 'afterbegin',
+    BEFOREEND: 'beforeend',
+    AFTEREND: 'afterend',
 };
-
-
-function createElement(template) 
-{
-  const newElement = document.createElement('div');
-  newElement.innerHTML = template;
-
-
-  return newElement.firstElementChild;
+  
+  
+function createElement(template) {
+    const newElement = document.createElement('div');
+    newElement.innerHTML = template;
+    return newElement.firstElementChild;
+}
+  
+  
+function render(component, container, place = RenderPosition.BEFOREEND) {
+  if (!(component instanceof AbstractComponent)) {
+    throw new Error('Can render only components');
+  }
+   if (container === null) {
+    throw new Error('Container element doesn\'t exist');
+  }
+   container.insertAdjacentElement(place, component.element);
 }
 
-
-function render(component, container, place = RenderPosition.BEFOREEND) 
-{
-container.insertAdjacentElement(place, component.getElement());
-}
-
-
+  
 export {RenderPosition, createElement, render};
