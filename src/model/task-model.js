604367b10f17db9de4d9f31c95dd05fb2b76d1a7
Ilnarey <ilnaragaliullina16@gmail.com>
2024-11-08 02:28:37 +0500

    Update task-model.js

--- src/model/task-model.js
+++ src/model/task-model.js
@@ -1,9 +1,41 @@
+import { generateId } from '../../utils.js';
 import { tasks } from '../mock/task.js';
 
 export default class TasksModel {
  #boardtasks = tasks;
+ #observers =[];
 
- getTasks() {
+ get tasks() {
    return this.#boardtasks;
+ }
+ getTasksByStatus(status){
+  return this.#boardtasks.filter(task=>task.status===status);
+ }
+
+ addTask(title){
+  const newTask = {
+    id: generateId(),
+    title,
+    status:'blogack',
+  };
+
+  this.#boardtasks.push(newTask);
+  this._notifyObservers();
+  return newTask;
+ }
+ clearTasksByStatus(status) {
+  this.#boardtasks = this.#boardtasks.filter(task => task.status !== status);
+}
+
+ addObserver(observer){
+  this.#observers.push(observer);
+ }
+
+ removeObserver(observer){
+  this.#observers=this.#observers.filter((obs) => obs !== observer);
+ }
+
+ _notifyObservers(){
+  this.#observers.forEach((observer) => observer());
  }
 }
