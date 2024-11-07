79076f24ec8e75c76ac3dbbce351ab77739d64b4
Ilnarey <ilnaragaliullina16@gmail.com>
2024-11-08 02:31:21 +0500

    Update button-delite.js

--- src/view/button-delite.js
+++ src/view/button-delite.js
@@ -6,9 +6,21 @@ function createClearButtonComponentTemplate() {
         `; 
 } 
  
-export default class ClearButtonComponent extends AbstractComponent{ 
-  get template() { 
-    return createClearButtonComponentTemplate(); 
-  } 
- 
+export default class ClearButtonComponent extends AbstractComponent {
+  #handleClick = null;
+
+  constructor({ onClick }) {
+    super();
+    this.#handleClick = onClick;
+    this.element.addEventListener("click", this.#clickHandler); 
+  }
+
+  get template() {
+    return createClearButtonComponentTemplate();
+  }
+
+  #clickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleClick();
+  };
 }
