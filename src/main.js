fa12cdc00ab2405b4c206588177a6c4e23917642
Ilnarey <ilnaragaliullina16@gmail.com>
2024-11-08 02:34:28 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,30 @@
 import HeaderComponent from './view/header-component.js';
-import FormComponent from './view/form-component.js';
+import FormAddTaskComponent from './view/form-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
+import TasksModel from "./model/task-model.js";
 import {render, RenderPosition} from './framework/render.js';
-import TasksModel from './model/task-model.js';
 
 const bodyContainer= document.querySelector('.header');
 const formContainer= document.querySelector('.new_task');
-const taskBlockContainer= document.querySelector('.tasks');
+const tasksBoardContainer= document.querySelector('.tasks');
 
 const tasksModel = new TasksModel();
+const headerComponent = new HeaderComponent();
 const tasksBoardPresenter = new TasksBoardPresenter({
-    boardContainer: taskBlockContainer, 
-    tasksModel,
+ boardContainer: tasksBoardContainer,
+ tasksModel,
+
 });
+const formAddTaskComponent = new FormAddTaskComponent({
+    onClick: handleNewTaskButtonClick,
+  });
 
+  function handleNewTaskButtonClick() {
+    tasksBoardPresenter.createTask();
+  }
 
-render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
-render(new FormComponent(), formContainer, RenderPosition.AFTERBEGIN);
+render(headerComponent, bodyContainer, RenderPosition.AFTERBEGIN);
+render(formAddTaskComponent, formContainer, RenderPosition.AFTERBEGIN);
+
 tasksBoardPresenter.init();
+
