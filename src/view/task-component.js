365657af97fcfed783a24cf96cf2ca13a0d357e7
Ilnarey <ilnaragaliullina16@gmail.com>
2024-11-10 18:04:53 +0500

    Update task-component.js

--- src/view/task-component.js
+++ src/view/task-component.js
@@ -16,9 +16,21 @@ import { AbstractComponent } from "../framework/view/abstract-component.js";
     constructor({ task }) {
       super();
         this.task = task;
+        this.#afterCreateElement();
       }
     
       get template() { 
         return createTaskComponentTemplate(this.task);
+      }
+
+      #afterCreateElement(){
+        this.#makeTaskDraggable();
+      }
+
+      #makeTaskDraggable(){
+        this.element.setAttribute('draggable',true);
+        this.element.addEventListener('dragstart',(event)=>{
+          event.dataTransfer.setData('text/plain',this.task.id)
+        })
       }
 }
