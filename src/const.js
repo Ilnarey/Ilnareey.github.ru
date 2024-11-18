94aad4acd25160d9628571bcf167c4f91addf1ed
Ilnarey <ilnaragaliullina16@gmail.com>
2024-11-18 18:01:33 +0500

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,15 +1,26 @@
 export const Status = {
-    BACKLOG: `blogack`,
-    PROCESSING: `in_progress`,
-    DONE: `ready`,
-    BASKET: `garbage`,
-  };
-  
-
+  BACKLOG: `blogack`,
+  PROCESSING: `in_progress`,
+  DONE: `ready`,
+  BASKET: `garbage`,
+};
 
 export const StatusLabel = {
-    [Status.BACKLOG]: `Бэклог`,
-    [Status.PROCESSING]: `В процессе`,
-    [Status.DONE]: `Готово`,
-    [Status.BASKET]: `Корзина`,
-  };
+  [Status.BACKLOG]: `Бэклог`,
+  [Status.PROCESSING]: `В процессе`,
+  [Status.DONE]: `Готово`,
+  [Status.BASKET]: `Корзина`,
+};
+
+export const  UserAction ={
+  UPDATE_TASK: 'UPDATE_TASK',
+  ADD_TASK: 'ADD_TASK',
+  DELETE_TASK: 'DELETE_TASK',
+};
+
+export const  UpdateType ={
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+  INIT: 'INIT',
+};
